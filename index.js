ca938819c6aecbf378cab0c953dabf074b6b69b4
Josito <joserr0209@gmail.com>
2021-10-05 12:45:58 +0200

    Chat-1: Add connection and disconnection handler

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const { nextTick } = require("process");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-let userConected = 0;
+let userConected = [];
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
 io.on("connection", (socket) => {
-  userConected++;
-  socket.emit("usersConected", userConected);
+  userConected.push(socket.id);
+  socket.on("disconnect", () => {
+      userConected.splice(userConected.indexOf(socket.id),1);
+      io.emit("usersConected", userConected.length);
+  });
+  io.emit("usersConected", userConected.length);
 });
+
+
 server.listen(3000, () => {
   console.log("listening on *:3000");
 });
