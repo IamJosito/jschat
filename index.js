6bd502c086b84e209e6fc20c343b5be3d09362bb
Josito <joserr0209@gmail.com>
2021-10-05 12:13:06 +0200

    Add socket IO

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 8080;
-const path = require('path');
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+let userConected = 0;
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(__dirname + "/index.html");
 });
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+io.on("connection", (socket) => {
+  userConected++;
+  socket.emit("usersConected", userConected);
+});
+server.listen(3000, () => {
+  console.log("listening on *:3000");
 });
